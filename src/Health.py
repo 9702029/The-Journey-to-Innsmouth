21609f888c6d3a49e7a4ed4c2db436b4cdd3db73
julia-strong <142455040+julia-strong@users.noreply.github.com>
2024-02-21 18:46:31 -0700

    Add files via upload

--- src/Health.py
+++
@@ -1,5 +0,0 @@
-class Health:
-    def __init__(self, Health):
-        self.Health = Health
-        self.Health_bar = "Health""[" + "=" * self.Health + " " * (10 - self.Health) + "]"
-    Health = 10
--- src/Sanity.py
+++
@@ -1,6 +0,0 @@
-Dif_Sanity=0
-class Sanity:
-    def __init__(self, Sanity):
-        self.Sanity = Sanity
-        self.Sanity_bar = "Sanity""[" + "=" * self.Sanity + " " * (10 - self.Sanity) + "]"
-    Sanity = 10
