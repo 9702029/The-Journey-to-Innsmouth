9bcee8890cb42f154fa9f2fb2279f0d5596381d8
julia-strong <9652160@graniteschools.org>
2024-02-28 16:41:23 -0700

    keyboard w/out getkey?

--- src/main.py
+++ src/main.py
@@ -4,8 +4,8 @@ from Player import Insanity
 from Player import Health
 from Random_encounter import RandomEncounter
 from Shop import Shop
-from getkey import getkey
-from getkey import keys
+#from getkey import getkey
+#from getkey import keys
 import keyboard
 from images import draw_image1
 from images import draw_image2
